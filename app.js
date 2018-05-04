0db0609b3e3aada456f3c0f807516bdc3e16fdab
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-05-04 12:28:59 +0300

    deleted "catch"

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import path from 'path';
+//import path from 'path';
 
 import { DirWatcher } from './models/dirwatcher';
 import { Importer } from './models/importer';
@@ -10,22 +10,16 @@ const directoryPath = './data';
 const delayForWatching = 3000;
 const delayForUnWatch = 50000;
 
-try {
-    dirWatcher.on('changed', (filePath) => {
-        importer
-            .import(filePath)
-            .then((data) => {
-                console.log(data[0]);
-            });
-    });
+dirWatcher.on('changed', (filePath) => {
+    importer
+        .import(filePath)
+        .then((data) => {
+            console.log(data[0]);
+        });
+    console.log(importer.importSync(filePath)[0]);
+});
 
-    dirWatcher.on('changed', (filePath) => {
-        console.log(importer.importSync(filePath)[0]);
-    });
-
-    dirWatcher.watch(directoryPath, delayForWatching);
-
-} catch (error) { }
+dirWatcher.watch(directoryPath, delayForWatching);
 
 setTimeout(() => {
     dirWatcher.unWatch();
