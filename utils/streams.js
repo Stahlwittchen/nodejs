e8f9f4f17c584946f55ec021ee852fb07a1021f2
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-05-18 16:34:30 +0300

    "transform" updated

--- utils/streams.js
+++ utils/streams.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const filePath = '../data/test.csv';
 const reader = fs.createReadStream(filePath);
-const through = require('through2')
+const through = require('through2');
 reader
     .pipe(through(reverse))
+    .pipe(through(transform))
     .pipe(process.stdout);
 
 function reverse(chunk, enc, callback) {
@@ -14,7 +15,11 @@ function reverse(chunk, enc, callback) {
     this.push(joinArr)
     callback();
 }
-// function transform(str) { /* ... */ }
+function transform(chunk, enc, callback) {
+    var transform = chunk.toString().toUpperCase();
+    this.push(transform)
+    callback();
+}
 // function outputFile(filePath) { /* ... */ }
 // function convertFromFile(filePath) { /* ... */ }
 // function convertToFile(filePath) { /* ... */ }
