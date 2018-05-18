6c79a929de39c97d51edb858b6081a45ffe3622b
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-05-18 16:30:33 +0300

    worked first subtask "reverse" ! =) Huraaaay

--- utils/streams.js
+++ utils/streams.js
@@ -3,14 +3,17 @@ const filePath = '../data/test.csv';
 const reader = fs.createReadStream(filePath);
 const through = require('through2')
 reader
-    .on('data', (data) => {
-        console.log(data.toString())
-    })
-    .on('end', function () {
-        console.log('done')
-    })
+    .pipe(through(reverse))
+    .pipe(process.stdout);
 
-//function reverse(str) {}
+function reverse(chunk, enc, callback) {
+    var splitString = chunk.toString().split("");
+    var reverseArr = splitString.reverse();
+    var joinArr = reverseArr.join("");
+
+    this.push(joinArr)
+    callback();
+}
 // function transform(str) { /* ... */ }
 // function outputFile(filePath) { /* ... */ }
 // function convertFromFile(filePath) { /* ... */ }
