31b41dc799ce116a1827c81840c15926d7188d80
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-05-29 14:43:24 +0300

    updated 3rd task

--- utils/streams.js
+++ utils/streams.js
@@ -1,25 +1,98 @@
 const fs = require('fs');
-const filePath = '../data/test.csv';
-const reader = fs.createReadStream(filePath);
 const through = require('through2');
-reader
-    .pipe(through(reverse))
-    .pipe(through(transform))
-    .pipe(process.stdout);
+const csv = require('csvtojson');
+const p = require('path');
 
-function reverse(chunk, enc, callback) {
-    var splitString = chunk.toString().split("");
-    var reverseArr = splitString.reverse();
-    var joinArr = reverseArr.join("");
 
-    this.push(joinArr)
-    callback();
+function reverse() {
+    process.stdin;
+
+    let toUpperCase = through(function (chunk, enc, callback) {
+        this.push(chunk.toString().split("").reverse().join("") + '\n');
+        callback();
+    });
+    process.stdin.pipe(toUpperCase).pipe(process.stdout)
 }
-function transform(chunk, enc, callback) {
-    var transform = chunk.toString().toUpperCase();
-    this.push(transform)
-    callback();
+
+function transform() {
+    process.stdin;
+
+    let toUpperCase = through(function (chunk, enc, callback) {
+        this.push(chunk.toString().toUpperCase());
+        callback();
+    });
+    process.stdin.pipe(toUpperCase).pipe(process.stdout)
 }
-// function outputFile(filePath) { /* ... */ }
-// function convertFromFile(filePath) { /* ... */ }
-// function convertToFile(filePath) { /* ... */ }
+
+function outputFile(file) {
+    exists(file);
+    let reader = fs.createReadStream(file);
+    reader.pipe(process.stdout);
+}
+
+function convertFromFile(file) {
+    exists(file);
+    let reader = fs.createReadStream(file);
+
+    reader.pipe(toJson(file)).pipe(process.stdout);
+}
+
+function convertToFile(file) {
+    exists(file);
+    let reader = fs.createReadStream(file);
+    let writer = fs.createWriteStream("../data/test.json");
+
+    reader.pipe(toJson(file)).pipe(writer);
+}
+
+const args = require('minimist')(process.argv.slice(2), {
+    string: ['action', 'file', 'path', 'help'],
+    alias: {a: 'action', f: 'file', p: 'path', h: 'help'}
+});
+
+function printHelp() {
+    console.info(
+        'Drop any command: \n' +
+        '-a, --action (reverse, transform, outputFile, convertFromFile, convertToFile, cssBundler)\n-f, ' +
+        '--file (path to file)\n-p,' +
+        ' --path (path to ...)'
+    );
+}
+
+function exists(filePath) {
+    fs.exists(filePath, function (exist) {
+        if (!exist) {
+            console.log("File not exists!");
+            process.exit();
+        }
+    })
+}
+
+const toJson = (file) => {
+    return csv().fromFile(file).on("end_parsed", function (jsonData) {
+        this.push(jsonData)
+    })
+};
+
+
+switch (args.action) {
+    case 'reverse':
+        reverse();
+    case 'transform':
+        transform();
+    case 'outputFile':
+        outputFile(args.file);
+        break;
+    case 'convertFromFile':
+        convertFromFile(args.file);
+        break;
+    case 'convertToFile':
+        convertToFile(args.file);
+        break;
+    case 'cssBundler':
+        cssBundler(args.path);
+        break;
+    default:
+        printHelp()
+}
+
